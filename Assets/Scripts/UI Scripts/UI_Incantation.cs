4e00525f7ece533659617aab85c21a6b4c572d19
akoreyu <134470009+akoreyu@users.noreply.github.com>
2025-10-23 20:37:47 -0400

    HUD Updated with Incantation

---
+++ Assets/Scripts/UI Scripts/UI_Incantation.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using static UnityEngine.Rendering.VolumeComponent;
+
+public class UI_Incantation : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI incantationText;
+    private string incantation;
+    LazyDependency<InputManager> _InputManager;
+    LazyDependency<SpellDatabase> _SpellDatabase;
+
+    private void Start()
+    {
+        if (incantationText == null) gameObject.GetComponent<TextMeshProUGUI>();
+        _InputManager.Value.OnGrimoireIncant += DisplayIncantation;
+        _InputManager.Value.OnStaffCast += ResetIncantation;
+        _InputManager.Value.OnStaffCancelIncant += ResetIncantation;
+    }
+
+    public void DisplayIncantation(string c)
+    {
+        incantation += c;
+        if (incantation.Length > 6)
+        {
+            ResetIncantation();
+        }
+        incantationText.text = incantation;
+    }
+
+    public void ResetIncantation()
+    {
+        incantation = "";
+        incantationText.text = incantation;
+    }
+}
